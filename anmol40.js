0062f10118d52e3fd5676b0b870d57c257a65baa
AnmolSingh221 <114210608+AnmolSingh221@users.noreply.github.com>
2023-02-16 21:03:15 +0530

    Create anmol40.js

---
+++ anmol40.js
@@ -0,0 +1,19 @@
+let a = null
+let b = 12;
+let c = "anmol";
+let d = Symbol("i m symbol")
+let e = BigInt("5679") + BigInt("2")
+let f = true
+let g = undefined
+
+console.log(a, b, c, d, e, f, g)
+
+// Objects in js
+const item =
+{
+  "Anmol": true,
+  "Harry": false,
+  "Rohan": 65,
+  "Vijay": undefined
+}
+console.log(item["Anmol"])
