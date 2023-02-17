0630282ae986b6c6ae68f5d45da31dc10f3d8c3d
AnmolSingh221 <114210608+AnmolSingh221@users.noreply.github.com>
2023-02-17 19:16:06 +0530

    Create anmol41.js

---
+++ anmol41.js
@@ -0,0 +1,16 @@
+console.log("JS Operators")
+
+let a=10
+let b=9
+
+console.log("a+b is",a+b)
+console.log("a-b is",a-b)
+console.log("a/b is",a/b)
+console.log("a%b is",a%b)
+console.log("a**b is",a**b)
+console.log("--b is",--b)
+
+// assignemnt operator
+
+console.log("a+5 is",a+=5)
+console.log("a/5 is",a/=5)
